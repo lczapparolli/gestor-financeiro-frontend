cba535329220fb8bc222d910a1af6ea083734b23
Luis Zapparolli <lczapparolli@gmail.com>
2026-06-10 19:52:57 +0000

    Ajustando menu inicial

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { ref, watch } from 'vue';
+
+const drawer = ref(false);
+const group = ref(null);
+
+watch(group, () => {
+  drawer.value = false;
+});
 </script>
 
 <template>
   <v-responsive class="rounded border">
     <v-app>
-      <v-app-bar title="Gestor Financeiro"></v-app-bar>
-
-      <v-navigation-drawer>
+      <v-app-bar>
+        <v-app-bar-title>Gestor Financeiro</v-app-bar-title>
+        <template v-slot:prepend>
+          <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        </template>
+      </v-app-bar>
+      <v-navigation-drawer v-model="drawer" :location="$vuetify.display.mobile ? 'bottom' : undefined" temporary>
         <v-list nav>
           <v-list-item title="Contas" to="/contas" link></v-list-item>
           <v-list-item title="Cartões" to="/cartoes" link></v-list-item>
